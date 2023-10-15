86a43402fb78b8f1a0b85ed76bded47e6f77ec6f
abhishekbhartiind <imabhishekbharti12@gmail.com>
2023-10-16 02:37:01 +0530

    bar chart practices'

--- src/App.js
+++ src/App.js
@@ -1,53 +1,116 @@
-import { useState } from 'react';
-import './App.css';
-import BarChart from './components/BarChart';
-import LineChart from './components/LineChart';
-import PieChart from './components/PieChart';
-import { UserData } from './Data';
+import { useState } from "react"
+import "./App.css"
+import BarChart from "./components/BarChart"
+import LineChart from "./components/LineChart"
+import PieChart from "./components/PieChart"
+import { UserData, BarData, BarData2 } from "./Data"
 
 function App() {
-	const [userData, setUserData] = useState({
-		labels: UserData.map((data) => data.year),
-		datasets: [
-			{
-				label: 'Users Gained',
-				data: UserData.map((data) => data.userGain),
-				// backgroundColor: [
-				// 	'rgba(75,192,192,1)',
-				// 	'#ecf0f1',
-				// 	'#50AF95',
-				// 	'#f3ba2f',
-				// 	'#2a71d0',
-				// ],
-				// borderColor: 'black',
-				// borderWidth: 2,
-				fill: 'start',
-				backgroundColor: (context) => {
-					const ctx = context.chart.ctx;
-					const gradient = ctx.createLinearGradient(0, 0, 0, 200);
-					gradient.addColorStop(0, '#24B337');
-					gradient.addColorStop(1, '#60EE73');
-					return gradient;
-				},
-			},
-		],
-	});
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        // backgroundColor: [
+        // 	'rgba(75,192,192,1)',
+        // 	'#ecf0f1',
+        // 	'#50AF95',
+        // 	'#f3ba2f',
+        // 	'#2a71d0',
+        // ],
+        // borderColor: 'black',
+        // borderWidth: 2,
+        fill: "start",
+        backgroundColor: (context) => {
+          const ctx = context.chart.ctx
+          const gradient = ctx.createLinearGradient(0, 0, 0, 200)
+          gradient.addColorStop(0, "#24B337")
+          gradient.addColorStop(1, "#60EE73")
+          return gradient
+        },
+      },
+    ],
+  })
 
-	// IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+  const [barData, setBarData] = useState({
+    labels: BarData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: BarData.map((data) => data.userGain),
+        fill: "start",
+        backgroundColor: (context) => {
+          const ctx = context.chart.ctx
+          const gradient = ctx.createLinearGradient(0, 0, 0, 200)
+          gradient.addColorStop(0, "#24B337")
+          gradient.addColorStop(1, "#60EE73")
+          return gradient
+        },
+      },
+    ],
+  })
 
-	return (
-		<div className="App">
-			<div style={{ width: 700 }}>
-				<BarChart chartData={userData} />
-			</div>
-			<div style={{ width: 700 }}>
-				<LineChart chartData={userData} />
-			</div>
-			<div style={{ width: 700 }}>
-				<PieChart chartData={userData} />
-			</div>
-		</div>
-	);
+  const [barData2, setBarData2] = useState({
+    labels: BarData2.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: BarData2.map((data) => data.userGain),
+        fill: "start",
+        borderWidth: 1,
+        hoverBackgroundColor: "#090979",
+        hoverBorderColor: "#090979",
+        backgroundColor: (context) => {
+          const ctx = context.chart.ctx
+          const gradient = ctx.createLinearGradient(0, 0, 0, 100)
+          gradient.addColorStop(0, "#090979")
+          gradient.addColorStop(1, "#00cbf9")
+          return gradient
+        },
+      },
+    ],
+    options: {
+      tooltips: {
+        displayColors: true,
+        position: "top",
+        labels: {
+          fontColor: "#000080",
+        },
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: { position: "bottom" },
+    },
+  })
+
+  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+
+  return (
+    <div className="App">
+      <div style={{ width: 700 }}>
+        <BarChart chartData={barData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <BarChart chartData={barData2} />
+      </div>
+      <div style={{ width: 700 }}>
+        <LineChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <PieChart chartData={userData} />
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/Data.js
+++ src/Data.js
@@ -29,4 +29,140 @@ export const UserData = [
     userGain: 4300,
     userLost: 234,
   },
-];
+]
+
+export const BarData = [
+  {
+    id: 1,
+    year: "01",
+    userGain: 4700,
+  },
+  {
+    id: 2,
+    year: "02",
+    userGain: 1000,
+  },
+  {
+    id: 3,
+    year: "03",
+    userGain: 3200,
+  },
+  {
+    id: 4,
+    year: "04",
+    userGain: 500,
+  },
+  {
+    id: 5,
+    year: "05",
+    userGain: 5200,
+  },
+  {
+    id: 6,
+    year: "06",
+    userGain: 3800,
+  },
+  {
+    id: 7,
+    year: "07",
+    userGain: 6000,
+  },
+  {
+    id: 8,
+    year: "08",
+    userGain: 4000,
+  },
+  {
+    id: 9,
+    year: "09",
+    userGain: 1500,
+  },
+  {
+    id: 10,
+    year: "10",
+    userGain: 4800,
+  },
+  {
+    id: 11,
+    year: "11",
+    userGain: 800,
+  },
+  {
+    id: 12,
+    year: "12",
+    userGain: 4300,
+  },
+  {
+    id: 13,
+    year: "13",
+    userGain: 1700,
+  },
+  {
+    id: 14,
+    year: "14",
+    userGain: 3000,
+  },
+  {
+    id: 15,
+    year: "15",
+    userGain: 2500,
+  },
+  {
+    id: 16,
+    year: "16",
+    userGain: 2100,
+  },
+  {
+    id: 17,
+    year: "17",
+    userGain: 4100,
+  },
+  {
+    id: 18,
+    year: "18",
+    userGain: 1900,
+  },
+  {
+    id: 19,
+    year: "19",
+    userGain: 3800,
+  },
+  {
+    id: 20,
+    year: "20",
+    userGain: 5100,
+  },
+]
+
+export const BarData2 = [
+  {
+    id: 1,
+    year: "Jul",
+    userGain: 6000,
+  },
+  {
+    id: 2,
+    year: "Aug",
+    userGain: 7650,
+  },
+  {
+    id: 3,
+    year: "Sep",
+    userGain: 3600,
+  },
+  {
+    id: 4,
+    year: "Oct",
+    userGain: 2500,
+  },
+  {
+    id: 5,
+    year: "Nov",
+    userGain: 5000,
+  },
+  {
+    id: 6,
+    year: "Dec",
+    userGain: 8600,
+  },
+]
